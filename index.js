3215341c8c7b27554f3742d04f13051f884521f5
Chantra555 <chantraphom555@gmail.com>
2026-02-03 15:11:42 -0500

    Part three complete

--- index.js
+++ index.js
@@ -43,4 +43,39 @@ console.log(`${this.name} rolled a ${result}.`)
 const newRobin = new Character("NewRobin")
 
 console.log(newRobin.roll());
+
+// note - better way to have done this instead of following exercise instruction was turn const Robin into class and extend it 
+
 //Part 3 Class Feautures 
+class Adventurer extends Character {
+  constructor (name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout () {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+
+class companion extends Adventurer{
+    constructor (name,type){
+        super(name);
+        this.type = type;
+        this.invetory.push("+ 50 health potion");
+    }
+    sheild (){
+        console.log(`${this.name} is protecting for this amt:`)
+        super.roll();
+    }
+
+}
+//change robin into class
+
+const robin = new Adventurer("Robin");
+const leo = new companion("Leo");
+const frank = new companion("Frank")
